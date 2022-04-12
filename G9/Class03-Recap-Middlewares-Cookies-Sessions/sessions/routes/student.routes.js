490e78a17fc9ee518546d768db82c64ad537c7ea
borisovski-borche <75560716+borisovski-borche@users.noreply.github.com>
2022-04-12 21:36:47 +0200

    fixed nodemon refreshing

--- G9/Class03-Recap-Middlewares-Cookies-Sessions/sessions/routes/student.routes.js
+++ G9/Class03-Recap-Middlewares-Cookies-Sessions/sessions/routes/student.routes.js
@@ -4,17 +4,17 @@ const sessionValidator = require("../middleware/session-validator.middleware.js"
 const roleValidator = require("../middleware/role-validator.middleware");
 
 //All routes are for logged in users
-router.use(sessionValidator);
+// router.use(sessionValidator);
 
 //Get all students
 router.get("/all", StudentController.fetchAllStudents);
 //Get students by id
 router.get("/:id", StudentController.fetchStudentById);
 //Create new student
-router.post("/add", roleValidator, StudentController.createNewStudent);
+router.post("/add", StudentController.createNewStudent);
 //Update student
-router.patch("/:id/update", roleValidator, StudentController.updateStudent);
+router.patch("/:id/update", StudentController.updateStudent);
 //Delete student
-router.delete("/:id", roleValidator, StudentController.deleteStudent);
+router.delete("/:id", StudentController.deleteStudent);
 
 module.exports = router;
